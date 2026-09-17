5db8c41f647a2b29ba7f9f9355b16591873564c0
Max Lin <sky1381@outlook.com>
2026-09-17 12:10:58 -0700

    Kinda fixed the problem

--- Lagrange Points/Main.cpp
+++ Lagrange Points/Main.cpp
@@ -1,19 +1,20 @@
-#include <Windows.h>
+#include <windows.h>
 #include <tchar.h>
 #include <cmath>
 #include <ctime>
 
 // The main window class name.
-static TCHAR szWindowClass[] = _T("Lagrange Point Calculator");
+static TCHAR szWindowClass[] = _T("DesktopApp");
 
 // The string that appears in the application's title bar.
-static TCHAR szTitle[] = _T("Lagrange Point Calculator");
+static TCHAR szTitle[] = _T("Windows Desktop Guided Tour Application");
 
 // Stored instance handle for use in Win32 API calls such as FindResource.
 HINSTANCE hInst;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+/*
 //Turns out C++'s base vector library isn't for math vectors:
 struct vector3 {
     float values[3] = { 0, 0, 0 };
@@ -53,15 +54,13 @@ class gravityObject {
     gravityObject() {
 
     };
-};
+};*/
 
-int WINAPI WinMain(
-    _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPSTR     lpCmdLine,
-    _In_ int       nCmdShow
-) {
-    // Some stuff for the window to work.
+int WINAPI WinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
     WNDCLASSEX wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
@@ -87,6 +86,20 @@ int WINAPI WinMain(
         return 1;
     }
 
+    // Store instance handle in our global variable
+    hInst = hInstance;
+
+    // The parameters to CreateWindowEx explained:
+    // WS_EX_OVERLAPPEDWINDOW : An optional extended window style.
+    // szWindowClass: the name of the application
+    // szTitle: the text that appears in the title bar
+    // WS_OVERLAPPEDWINDOW: the type of window to create
+    // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+    // 500, 100: initial size (width, height)
+    // NULL: the parent of this window
+    // NULL: this application doesn't have a menu bar
+    // hInstance: the first parameter from WinMain
+    // NULL: not used in this application
     HWND hWnd = CreateWindowEx(
         WS_EX_OVERLAPPEDWINDOW,
         szWindowClass,
@@ -99,6 +112,7 @@ int WINAPI WinMain(
         hInstance,
         NULL
     );
+
     if (!hWnd)
     {
         MessageBox(NULL,
@@ -109,20 +123,50 @@ int WINAPI WinMain(
         return 1;
     }
 
+    // The parameters to ShowWindow explained:
+    // hWnd: the value returned from CreateWindowEx
+    // nCmdShow: the fourth parameter from WinMain
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
-    //Prevent the window from instant closing.
-    //while (true) {
-    //    Sleep(1);
-    //}
-};
+    // Main message loop:
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
+    return (int)msg.wParam;
+}
+
+//When a button is pressed
 LRESULT CALLBACK WndProc(
-    HWND hWnd,
-    UINT message,
-    WPARAM wParam,
-    LPARAM lParam
+    _In_ HWND   hWnd,
+    _In_ UINT   message,
+    _In_ WPARAM wParam,
+    _In_ LPARAM lParam
 ) {
+    PAINTSTRUCT ps;
+    HDC hdc;
+    TCHAR greeting[] = _T("Hello, Windows desktop!");
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+
+        // Here your application is laid out.
+        // For this introduction, we just print out "Hello, Windows desktop!"
+        // in the top left corner.
+        TextOut(hdc,
+            5, 5,
+            greeting, static_cast<int>(_tcslen(greeting))); // TextOut expects an int here but _tcslen returns a size_t. Thus the cast.
+        // End application-specific layout section.
+
+        EndPaint(hWnd, &ps);
+        break;
+    }
+
     return 0;
 };
